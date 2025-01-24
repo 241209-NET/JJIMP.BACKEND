8f18c604a0c2d9e34cea4fdadb0a4e6d93f5a220
JtheFox <60980739+JtheFox@users.noreply.github.com>
2025-01-24 11:24:10 -0500

    Create issue and comment controllers

--- JJIMP.API/2_Controller/CommentController.cs
+++ JJIMP.API/2_Controller/CommentController.cs
@@ -8,4 +8,38 @@ namespace JJIMP.API.Controller;
 [ApiController]
 public class CommentController : ControllerBase
 {
+    private readonly ICommentService _commentService;
+
+    public CommentController(ICommentService commentService)
+    {
+        _commentService = commentService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult> GetComment(int id)
+    {
+        var comment = await _commentService.GetCommentById(id);
+        return Ok(comment);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateComment(CreateCommentDTO commentDTO)
+    {
+        var comment = await _commentService.CreateComment(commentDTO);
+        return Ok(comment);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateComment(UpdateCommentDTO commentDTO)
+    {
+        var comment = await _commentService.UpdateComment(commentDTO);
+        return Ok(comment);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteComment(int id)
+    {
+        var comment = await _commentService.DeleteComment(id);
+        return Ok(comment);
+    }
 }
--- JJIMP.API/2_Controller/IssueController.cs
+++ JJIMP.API/2_Controller/IssueController.cs
@@ -8,4 +8,47 @@ namespace JJIMP.API.Controller;
 [ApiController]
 public class IssueController : ControllerBase
 {
+    private readonly IIssueService _issueService;
+    private readonly ICommentService _commentService;
+
+    public IssueController(IIssueService issueService, ICommentService commentService)
+    {
+        _issueService = issueService;
+        _commentService = commentService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult> GetIssue(int id)
+    {
+        var issue = await _issueService.GetIssueById(id);
+        return Ok(issue);
+    }
+
+    [HttpGet("{id}/comments")]
+    public async Task<ActionResult> GetIssueComments(int id)
+    {
+        var comments = await _commentService.GetCommentsByIssueId(id);
+        return Ok(comments);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateIssue(CreateIssueDTO issueDTO)
+    {
+        var issue = await _issueService.CreateIssue(issueDTO);
+        return Ok(issue);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateIssue(UpdateIssueDTO issueDTO)
+    {
+        var issue = await _issueService.UpdateIssue(issueDTO);
+        return Ok(issue);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteIssue(int id)
+    {
+        var issue = await _issueService.DeleteIssue(id);
+        return Ok(issue);
+    }
 }
