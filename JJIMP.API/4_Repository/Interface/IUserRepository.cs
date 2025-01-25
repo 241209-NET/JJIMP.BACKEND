cae3251f093a741c7fe013ec124a795baba54707
Isaac Hessel-Robinson <isaacHhyphenR@gmail.com>
2025-01-25 11:44:16 -0600

    Filled out user repository

--- JJIMP.API/4_Repository/Interface/IUserRepository.cs
+++ JJIMP.API/4_Repository/Interface/IUserRepository.cs
@@ -4,5 +4,10 @@ namespace JJIMP.API.Repository;
 
 public interface IUserRepository
 {
-    
+    Task<User> CreateUser(User user);
+    Task<User?> DeleteUserById(int userId);
+    Task<User?> UpdateUser(User user);
+    Task<User?> GetUserById(int userId);
+    Task<IEnumerable<User>> GetAllUsers();
+
 }
--- JJIMP.API/4_Repository/UserRepository.cs
+++ JJIMP.API/4_Repository/UserRepository.cs
@@ -1,9 +1,49 @@
+using System.Threading.Tasks;
 using JJIMP.API.Data;
 using JJIMP.API.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace JJIMP.API.Repository;
 
 public class UserRepository : IUserRepository
 {
+    private readonly JjimpContext _dbContext;
+
+    public UserRepository(JjimpContext dbContext) => _dbContext = dbContext;
+
+    public async Task<User> CreateUser(User user)
+    {
+        await _dbContext.Users.AddAsync(user);
+        await _dbContext.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task<User?> DeleteUserById(int userId)
+    {
+        var user = await GetUserById(userId);
+        if(user == null)
+        {
+            return null;
+        }
+        _dbContext.Users.Remove(user);
+        await _dbContext.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task<User?> UpdateUser(User user)
+    {
+        _dbContext.Users.Update(user);
+        await _dbContext.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task<User?> GetUserById(int userId)
+    {
+        return await _dbContext.Users.FindAsync(userId);
+    }
+    public async Task<IEnumerable<User>> GetAllUsers()
+    {
+        return await _dbContext.Users.ToListAsync();
+    }
 
 }
