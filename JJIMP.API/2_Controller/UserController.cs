aaafd3c9fb6fa11aea426a17b204558f0af05988
Isaac Hessel-Robinson <isaacHhyphenR@gmail.com>
2025-01-25 20:51:13 -0600

    Created user controller

--- JJIMP.API/2_Controller/UserController.cs
+++ JJIMP.API/2_Controller/UserController.cs
@@ -8,4 +8,38 @@ namespace JJIMP.API.Controller;
 [ApiController]
 public class UserController : ControllerBase
 {
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult> GetUser(int id)
+    {
+        var user = await _userService.GetUserById(id);
+        return Ok(user);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateUser(UserInDTO userDTO)
+    {
+        var user = await _userService.CreateUser(userDTO);
+        return Ok(user);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(UserInDTO userDTO)
+    {
+        var user = await _userService.UpdateUser(userDTO);
+        return Ok(user);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteUser(int id)
+    {
+        var user = await _userService.DeleteUserById(id);
+        return Ok(user);
+    }
 }
