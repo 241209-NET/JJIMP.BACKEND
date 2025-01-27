55aa3e95170c09e10b603e0218ba4fd945f8ca1b
JtheFox <60980739+JtheFox@users.noreply.github.com>
2025-01-27 10:01:07 -0500

    Add fixture init to test constructors

--- JJIMP.Tests/CommentServiceTests.cs
+++ JJIMP.Tests/CommentServiceTests.cs
@@ -13,12 +13,20 @@ public class CommentServiceTests
     private Mock<ICommentRepository> _commentRepositoryMock;
     private Mock<IMapper> _mapperMock;
     private CommentService _commentService;
+    private Fixture _fixture;
 
     public CommentServiceTests()
     {
         _commentRepositoryMock = new Mock<ICommentRepository>();
         _mapperMock = new Mock<IMapper>();
         _commentService = new CommentService(_commentRepositoryMock.Object, _mapperMock.Object);
+
+        _fixture = new Fixture();
+        _fixture.Customize<DateOnly>(o => o.FromFactory((DateTime dt) => DateOnly.FromDateTime(dt)));
+        _fixture.Customize<TimeOnly>(o => o.FromFactory((DateTime dt) => TimeOnly.FromDateTime(dt)));
+        _fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+    .ForEach(b => _fixture.Behaviors.Remove(b));
+        _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
     }
 
 
--- JJIMP.Tests/ProjectServiceTests.cs
+++ JJIMP.Tests/ProjectServiceTests.cs
@@ -13,12 +13,20 @@ public class ProjectServiceTests
     private Mock<IProjectRepository> _projectRepositoryMock;
     private Mock<IMapper> _mapperMock;
     private ProjectService _projectService;
+    private Fixture _fixture;
 
     public ProjectServiceTests()
     {
         _projectRepositoryMock = new Mock<IProjectRepository>();
         _mapperMock = new Mock<IMapper>();
         _projectService = new ProjectService(_projectRepositoryMock.Object, _mapperMock.Object);
+
+        _fixture = new Fixture();
+        _fixture.Customize<DateOnly>(o => o.FromFactory((DateTime dt) => DateOnly.FromDateTime(dt)));
+        _fixture.Customize<TimeOnly>(o => o.FromFactory((DateTime dt) => TimeOnly.FromDateTime(dt)));
+        _fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+    .ForEach(b => _fixture.Behaviors.Remove(b));
+        _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
     }
 
 
